8e108a5305d150635f7a972ea05af187dff7f02d
Garung <canmotcaiten1993@gmail.com>
2017-09-07 00:33:43 +0700

    test for 3 menus

--- src/menu/vc-menu.js
+++ src/menu/vc-menu.js
@@ -1,24 +1,41 @@
-import './jquery.meanmenu/jquery.meanmenu.min.js';
+require('./jquery.meanmenu/jquery.meanmenu.min.js');
 
 //for menu1
 $(document).ready(function () {
-    $('.vc-class-menu1 .main-menu').meanmenu({
+    $('.vc-class-menu1 .mean-menu1').meanmenu({
         meanScreenWidth: "992",
-        meanMenuContainer: ".mobile-menu",
+        meanMenuContainer: ".mobile-vc-menu1",
     });
 
     var scroll = $('div').width();
     if(scroll >= 992) {
-        var height = $('.vc-class-menu1 .main-menu > ul > li .sub-menu li .sub-menu');
+        var height = $('.vc-class-menu1 .mean-menu1 > ul > li .sub-menu li .sub-menu');
         var height2 = height.height();
-        var $content = $('.vc-class-menu1 .main-menu > ul > li .sub-menu');
+        var $content = $('.vc-class-menu1 .mean-menu1 > ul > li .sub-menu');
         $content.height(height2);
     }
 
-    $('.vc-class-menu1 .main-menu > ul > li').hover(function(){
+    $('.vc-class-menu1 .mean-menu1 > ul > li').hover(function(){
         $(this).find('.sub-menu > li:first-child > .sub-menu').css('visibility', 'visible');
     },function(e) {
         $(this).find('.sub-menu > li:first-child > .sub-menu').css('visibility', 'hidden');
     });
         
+    // for menu2 
+    $('.vc-class-menu2 .mean-menu2').meanmenu({
+        meanScreenWidth: "992",
+        meanMenuContainer: ".mobile-vc-menu2",
+    });
+
+    // for menu3
+    $('.vc-class-menu3 .mean-menu3').meanmenu({
+        meanScreenWidth: "1030",
+        meanMenuContainer: ".mobile-vc-menu3",
+    });
+
+    $('.vc-class-menu3 .mean-menu3 > ul > li').hover(function(){
+        $(this).find('.sub-menu > li:first-child > .sub-menu').css('visibility', 'visible');
+    },function(e) {
+        $(this).find('.sub-menu > li:first-child > .sub-menu').css('visibility', 'hidden');
+    });
 });
